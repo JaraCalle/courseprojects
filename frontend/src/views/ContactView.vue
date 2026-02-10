13f4286daedf95ff6854481b85e3566245e72325
Juan Jara <jaracalle2041@gmail.com>
2026-02-10 17:34:25 -0500

    Feat Tutorial 03

---
+++ frontend/src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="mt-4 mb-4">
+    <div class="max-w-7xl mx-auto">
+      <h2 class="text-3xl font-bold text-gray-800 text-center">Contact Page</h2>
+      <img
+        src="https://www.infobae.com/new-resizer/gn45k3Kdtfr5q5XoyoO43kHUUCE=/arc-anglerfish-arc2-prod-infobae/public/2VAVZLYFLZCGHGWZOBM4G4X7FA.gif"
+        alt=""
+      />
+    </div>
+  </section>
+</template>
